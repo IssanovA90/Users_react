f0ad78066a5f60bfa11e577d258ed94d192d1717
IssanovA90 <aslan.isanov.90@mail.ru>
2023-10-02 15:05:25 +0600

    first commit

--- App.js
+++ App.js
@@ -1,70 +1,41 @@
-import * as React from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import RegistrationScreen  from "./components/RegistrationScreen";
-import EnterScreen from "./components/EnterScreen";
+import { SignInScreen } from './components/SignInScreen';
+import { SignUpScreen } from './components/SignUpScreen'
+import { ToDoApp } from "./components/ToDoApp";
+import { AuthorizationScreen} from'./components/AuthorizationScreen';
 
-const Stack = createNativeStackNavigator();
-
-function AuthScreen({navigation}) {
-  function onPress() {}
-  return (
-    <View style={styles.conteiner}>
-      <TouchableOpacity style={styles.button}  onPress={() =>
-        navigation.navigate('Регистрация', {name: 'Регистрация'})
-      }>
-        <Text style={styles.text}>Регистрация</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() =>
-        navigation.navigate('Войти', {name: 'Войти'})
-      }>
-        <Text style={styles.text}>Войти</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="AuthorizationScreen">
         <Stack.Screen
-          name="Авторизация"
-          component={AuthScreen}
-          options={{ title: "Авторизация" }}
+          name="SignUpScreen"
+          component={SignUpScreen}
+          options={{ title: 'Регистрация' }}
         />
         <Stack.Screen
-          name="Войти"
-          component={EnterScreen}
-          options={{ title: "Войти" }}
+          name="SignInScreen"
+          component={SignInScreen}
+          options={{ title: 'Вход' }}
         />
         <Stack.Screen
-          name="Регистрация"
-          component={RegistrationScreen}
-          options={{ title: "Регистрация" }}
+          name="AuthorizationScreen"
+          component={AuthorizationScreen}
+          options={{ title: 'Авторизация' }}
+        />
+        <Stack.Screen
+          name="ToDoScreen"
+          component={ToDoApp}
+          options={{ title: 'Задачи' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  conteiner: {
-    padding: 20,
-    gap: 20,
-  },
-  button: {
-    width: 300,
-    height: 50,
-    backgroundColor: "blue",
-    borderRadius: 10,
-    justifyContent: "center",
-    padding: 10,
-  },
-  text: {
-    color: "white",
-    fontWeight: "bold",
-  },
-});
+
 export default App;
